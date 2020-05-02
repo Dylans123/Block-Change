f02f023264c0a71649e1af0b4d58560140a586c2
dylans123 <dylanskelly@knights.ucf.edu>
2020-05-02 18:32:11 -0400

    Iterated on exisiting petition contract

--- backend/contracts/PetitionList.sol
+++ backend/contracts/PetitionList.sol
@@ -1,20 +1,50 @@
-pragma solidity ^0.6.4;
+pragma solidity ^0.5.0;
 
 contract PetitionList {
-  uint public taskCount = 0;
+  uint public petitionCount = 0;
+  mapping(uint => Petition) public petitions;
 
   struct Petition {
     uint id;
+    uint voteCount;
     string title;
     string description;
     Vote[] votes;
+    // VoteList voteList;
   }
 
   struct Vote {
     uint id;
     string firstName;
     string lastName;
+    string email;
   }
 
-  mapping(uint => Petition) public petition;
+  event PetitionCreated(
+    uint id,
+    string title,
+    string description
+  );
+
+  event VoteCreated(
+    uint id,
+    string firstName,
+    string lastName,
+    string email
+  );
+
+  function createPetition(string memory _title, string memory _description) public {
+    petitionCount ++;
+    petitions[petitionCount].title = _title;
+    petitions[petitionCount].description = _description;
+    petitions[petitionCount].voteCount = 1;
+    petitions[petitionCount].votes.push(Vote(petitionCount, "first", "last", "firstlast@gmail.com"));
+    emit PetitionCreated(petitionCount, _title, _description);
+  }
+
+  function CreateVote(uint _id, string memory _firstName, string memory _lastName, string memory _email) public {
+    petitions[_id].voteCount ++;
+    petitions[_id].votes.push(Vote(petitions[_id].voteCount, _firstName, _lastName, _email));
+    emit VoteCreated(_id, _firstName, _lastName, _email);
+  }
 }
