cdc7c95ac3a425e080ecf37462bdf7bf3a92866f
dylans123 <dylanskelly@knights.ucf.edu>
2020-05-02 19:36:49 -0400

    Began connecting front end to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Web3 from 'web3'
 import './App.css'
+import { PETITION_LIST_ABI, PETITION_LIST_ADDRESS } from './config'
 
 class App extends Component {
   componentDidMount() {
@@ -12,21 +13,27 @@ class App extends Component {
       console.log("woah")
       window.web3 = new Web3(window.ethereum);
       try {
-          // Request account access if needed
           await window.ethereum.enable();
-          // Acccounts now exposed
           const accounts = await window.web3.eth.getAccounts()
-          console.log(accounts)
           this.setState({ account: accounts[0] })
+          const petitionList = new window.web3.eth.Contract(PETITION_LIST_ABI, PETITION_LIST_ADDRESS)
+          this.setState({ petitionList })
+          console.log(petitionList)
+          const petitionCount = await petitionList.methods.petitionCount.call()
+          console.log("petition count: " + petitionCount)
       } catch (error) {
-          // User denied account access...
+          console.log(error)
       }
     }
   }
 
   constructor(props) {
     super(props)
-    this.state = { account: '' }
+    this.state = {
+      account: '',
+      petitionList: null,
+      petitionCount: 0
+    }
   }
 
   render() {
---
+++ frontend/src/config.js
@@ -0,0 +1,174 @@
+export const PETITION_LIST_ADDRESS = "0xF2417D1B61Cf03811764914271Ec9da9076AC0A4"
+export const PETITION_LIST_ABI = [
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "petitions",
+    "outputs": [
+      {
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "name": "voteCount",
+        "type": "uint256"
+      },
+      {
+        "name": "title",
+        "type": "string"
+      },
+      {
+        "name": "description",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function",
+    "signature": "0x3ad50ded"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "petitionCount",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function",
+    "signature": "0x61ab1f09"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "title",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "name": "description",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "name": "creatorFirstName",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "name": "creatorLastName",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "name": "creatorEmail",
+        "type": "string"
+      }
+    ],
+    "name": "PetitionCreated",
+    "type": "event",
+    "signature": "0x492297c097afce05595d715e035fab336852484c58ef49477673ae2a8a3ea14f"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "firstName",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "name": "lastName",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "name": "email",
+        "type": "string"
+      }
+    ],
+    "name": "VoteCreated",
+    "type": "event",
+    "signature": "0xe33023e944db00210b9bcd79a869ad15ab8b6c9a54f3e61d1484f922e79c6017"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_title",
+        "type": "string"
+      },
+      {
+        "name": "_description",
+        "type": "string"
+      },
+      {
+        "name": "_creatorFirstName",
+        "type": "string"
+      },
+      {
+        "name": "_creatorLastName",
+        "type": "string"
+      },
+      {
+        "name": "_creatorEmail",
+        "type": "string"
+      }
+    ],
+    "name": "createPetition",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function",
+    "signature": "0x5f248b1a"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_id",
+        "type": "uint256"
+      },
+      {
+        "name": "_firstName",
+        "type": "string"
+      },
+      {
+        "name": "_lastName",
+        "type": "string"
+      },
+      {
+        "name": "_email",
+        "type": "string"
+      }
+    ],
+    "name": "CreateVote",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function",
+    "signature": "0xb5023cc8"
+  }
+]
