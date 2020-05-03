c83b1529ccf64f3ec1eff1e3054d603ea0f19021
dylans123 <dylanskelly@knights.ucf.edu>
2020-05-03 02:07:24 -0400

    Updated header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { AppBar, Typography, Container } from "@material-ui/core"
+import { AppBar, Typography, Container, Button } from "@material-ui/core"
 
-const Header = () => {
-
+const Header = ({ handleOpen }) => {
   return (
     <AppBar position="static" style={{ backgroundColor: '#B4E178', color: 'black' }}>
       <Container maxWidth="sm">
         <div className="my-3" style={{ display: 'flex', alignItems: 'center' }}>
           <Typography variant="h5">
-            Blockchange
+            BLOCKCHANGE
           </Typography>
-          <Typography className="ml-5">Create a new petition</Typography>
-          <Typography className="ml-5">About</Typography>
-          <Typography className="ml-5">Contact</Typography>
+          <Button className="ml-4 p-0" style={{ textTransform: 'none' }} onClick={handleOpen()}>Create petition</Button>
+          <Button className="ml-4" style={{ textTransform: 'none' }}>About</Button>
+          <Button className="ml-4" style={{ textTransform: 'none' }}>Contact</Button>
         </div>
       </Container>
     </AppBar>
