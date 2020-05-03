c61ce7c6ddb5a8bebc2ead3e02db16f6597ce72d
dylans123 <dylanskelly@knights.ucf.edu>
2020-05-03 01:08:21 -0400

    Converted sign modal to be a class component and state controlled

--- frontend/src/components/SignModal.js
+++ frontend/src/components/SignModal.js
@@ -1,42 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField } from '@material-ui/core';
 
-const SignModal = ({ open, handleClose, petition }) => {
-  console.log(petition);
-  return (
-    <Dialog
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description"
-    >
-      <DialogTitle id="alert-dialog-title">{"Type your information below to sign "} {petition != null ? petition.title : null }</DialogTitle>
-      <DialogContent>
-        <form>
-          <div class="form-group">
-            <label for="firstName">First Name</label>
-            <input type="text" class="form-control" id="firstName" aria-describedby="emailHelp" placeholder="First Name" />
-          </div>
-          <div class="form-group">
-            <label for="lastName">Last Name</label>
-            <input type="text" class="form-control" id="lastName" aria-describedby="emailHelp" placeholder="Last Name" />
-          </div>
-          <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" />
-          </div>
-        </form>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>
-          Cancel
-        </Button>
-        <Button onClick={handleClose} autoFocus>
-          Sign
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+class SignModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: ''
+    }
+  }
+
+  handleChange = (e) => { this.setState({ [e.target.name]: e.target.value }) }
+
+  render() {
+    const { open, handleClose, petition } = this.props;
+    return (
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Type your information below to sign "} {petition != null ? petition.title : null }</DialogTitle>
+        <DialogContent>
+          <form>
+            <div class="form-group">
+              <label for="firstName">First Name</label>
+              <input type="text" class="form-control" id="firstName" name="firstName" onChange={(e) => {this.handleChange(e)}} placeholder="First Name" />
+            </div>
+            <div class="form-group">
+              <label for="lastName">Last Name</label>
+              <input type="text" class="form-control" id="lastName" name="lastName" onChange={(e) => {this.handleChange(e)}} placeholder="Last Name" />
+            </div>
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="email" class="form-control" id="email" name="email" onChange={(e) => {this.handleChange(e)}} placeholder="Email" />
+            </div>
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button onClick={handleClose} autoFocus>
+            Sign
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
 }
 
 export default SignModal;
