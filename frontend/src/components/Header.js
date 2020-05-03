6b93783126ac0ad7600f556b6372e1488a408b07
dylans123 <dylanskelly@knights.ucf.edu>
2020-05-03 02:54:23 -0400

    Added logo to site

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react'
+import Logo from '../assets/Logo.png'
 import { AppBar, Typography, Container, Button } from "@material-ui/core"
 
 const Header = ({ handleOpen }) => {
@@ -6,9 +7,10 @@ const Header = ({ handleOpen }) => {
     <AppBar position="static" style={{ backgroundColor: '#B4E178', color: 'black' }}>
       <Container maxWidth="sm">
         <div className="my-3" style={{ display: 'flex', alignItems: 'center' }}>
-          <Typography variant="h5">
+          {/* <Typography variant="h5">
             BLOCKCHANGE
-          </Typography>
+          </Typography> */}
+          <img src={Logo} />
           <Button className="ml-4 p-0" style={{ textTransform: 'none' }} onClick={() => handleOpen()}>Create petition</Button>
           <Button className="ml-4" style={{ textTransform: 'none' }}>About</Button>
           <Button className="ml-4" style={{ textTransform: 'none' }}>Contact</Button>
